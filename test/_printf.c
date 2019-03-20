8be2685ccc084aa288f50d6f35c3a200ae656896
ajdebiase <ajdebiase@gmail.com>
2019-03-20 01:19:30 +0000

    adding strlen to _printf.c

--- test/_printf.c
+++ test/_printf.c
@@ -10,3 +10,19 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+  * _strlen - Function to determine length of string.
+  * @str: Input string to determine length of.
+  * Return: Integer of value of strlen.
+  */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
